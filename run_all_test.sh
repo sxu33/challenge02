3d7bd18078adc24d38b4b9d1879767a50bba3c1c
sxu <sxu@szx>
2024-05-21 23:28:14 -0500

    add some fix

---
+++ run_all_test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+ 
+ruby ./01-hello-world/hello_world_test.rb
+ruby ./02-hello-world-instance/hello_world_test.rb
+ruby ./03-leap/leap_test.rb
+ruby ./04-pangram/pangram_test.rb
+ruby ./05-grains/grains_test.rb
